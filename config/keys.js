ad65f4c3b188c9d0ccfa93071fd05ee5cf06af38
blackhatplay <mannuall4u@gmail.com>
2021-03-17 18:50:36 +0530

    first commit

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // mongoURI: "mongodb://localhost:27017/articles",
-  mongoURI:
-    "mongodb+srv://blackhat:anonymous4u@cluster0.xvyhj.mongodb.net/articles?retryWrites=true&w=majority",
-};
